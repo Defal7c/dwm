a69dcc47719e7806e9bd22c8c49cf82be1f1ada2
defal7c <mth.fe.a@protonmail.com>
2022-02-26 09:44:49 -0300

    Update config.h
    Removed tmux

--- config.h
+++ config.h
@@ -68,7 +68,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_blue_sky, "-sf", col_gray4, NULL };
 //static const char *termcmd[]  = { "st", NULL };
-static const char *termcmd[]  = { "st", "-e", "tmux", NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
